f7a18968273cb2479567e365f009ba480043d44b
matthew-compton <compton@bignerdranch.com>
2014-10-14 11:27:10 -0400

    Added more information to the detail view, put it in a scroll view, and changed padding to be better

--- app/src/main/java/com/ambergleam/petfinder/controller/DetailFragment.java
+++ app/src/main/java/com/ambergleam/petfinder/controller/DetailFragment.java
@@ -24,6 +24,16 @@ public class DetailFragment extends BaseFragment {
 
     @InjectView(R.id.layout_name) LinearLayout mNameLayout;
     @InjectView(R.id.pet_name) TextView mNameTextView;
+    @InjectView(R.id.layout_animal) LinearLayout mAnimalLayout;
+    @InjectView(R.id.pet_animal) TextView mAnimalTextView;
+    @InjectView(R.id.layout_mix) LinearLayout mMixLayout;
+    @InjectView(R.id.pet_mix) TextView mMixTextView;
+    @InjectView(R.id.layout_age) LinearLayout mAgeLayout;
+    @InjectView(R.id.pet_age) TextView mAgeTextView;
+    @InjectView(R.id.layout_size) LinearLayout mSizeLayout;
+    @InjectView(R.id.pet_size) TextView mSizeTextView;
+    @InjectView(R.id.layout_description) LinearLayout mDescriptionLayout;
+    @InjectView(R.id.pet_description) TextView mDescriptionTextView;
 
     private Pet mPet;
 
@@ -70,14 +80,64 @@ public class DetailFragment extends BaseFragment {
 
     private void updateUI() {
         updateName();
+        updateAnimal();
+        updateMix();
+        updateAge();
+        updateSize();
+        updateDescription();
     }
 
     private void updateName() {
-        if (mPet.mName != null && !mPet.mName.mString.equals("")) {
+        if (mPet.mName.mString != null && !mPet.mName.mString.equals("")) {
             mNameLayout.setVisibility(View.VISIBLE);
             mNameTextView.setText(mPet.mName.mString);
         } else {
             mNameLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateAnimal() {
+        if (mPet.mAnimal.mString != null && !mPet.mAnimal.mString.equals("")) {
+            mAnimalLayout.setVisibility(View.VISIBLE);
+            mAnimalTextView.setText(mPet.mAnimal.mString);
+        } else {
+            mAnimalLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateMix() {
+        if (mPet.mMix.mString != null && !mPet.mMix.mString.equals("")) {
+            mMixLayout.setVisibility(View.VISIBLE);
+            mMixTextView.setText(mPet.mMix.mString);
+        } else {
+            mMixLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateAge() {
+        if (mPet.mAge.mString != null && !mPet.mAge.mString.equals("")) {
+            mAgeLayout.setVisibility(View.VISIBLE);
+            mAgeTextView.setText(mPet.mAge.mString);
+        } else {
+            mAgeLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateSize() {
+        if (mPet.mSize.mString != null && !mPet.mSize.mString.equals("")) {
+            mSizeLayout.setVisibility(View.VISIBLE);
+            mSizeTextView.setText(mPet.mSize.mString);
+        } else {
+            mSizeLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateDescription() {
+        if (mPet.mDescription.mString != null && !mPet.mDescription.mString.equals("")) {
+            mDescriptionLayout.setVisibility(View.VISIBLE);
+            mDescriptionTextView.setText(mPet.mDescription.mString);
+        } else {
+            mDescriptionLayout.setVisibility(View.INVISIBLE);
         }
     }
 
