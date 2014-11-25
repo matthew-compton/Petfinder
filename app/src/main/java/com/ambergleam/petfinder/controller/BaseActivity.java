11beafcaa0ab972b554bc99da635e10f041aca8b
matthew-compton <compton@bignerdranch.com>
2014-11-25 14:46:02 -0500

    Added new Overview Screen visuals

--- app/src/main/java/com/ambergleam/petfinder/controller/BaseActivity.java
+++ app/src/main/java/com/ambergleam/petfinder/controller/BaseActivity.java
@@ -1,11 +1,17 @@
 package com.ambergleam.petfinder.controller;
 
+import android.app.ActivityManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.ViewConfiguration;
 
@@ -26,12 +32,28 @@ public abstract class BaseActivity extends ActionBarActivity {
         PetfinderApplication.get(BaseActivity.this).inject(this);
         setContentView(R.layout.activity_fragment);
 
+        // Use toolbar as an action bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Enable app icon being shown on toolbar
         if (!(this instanceof MainActivity)) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             toolbar.setNavigationIcon(R.drawable.button_back);
+        }
+
+        // Custom look for overview screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int color = typedValue.data;
+
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.bowl);
+            ActivityManager.TaskDescription td = new ActivityManager.TaskDescription(null, bm, color);
+
+            setTaskDescription(td);
+            bm.recycle();
         }
 
         FragmentManager fm = getSupportFragmentManager();
